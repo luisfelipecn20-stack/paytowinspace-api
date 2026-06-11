7f7db8dfad78f38d8143b00270dc73c001c1837b
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-11 14:27:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ return {"mensaje": "Hola desde PaytowinSpace"}
 @app.post("/procesar")
 def procesar(datos: DatosEntrada):
 
-```
 return {
     "resultado": "OK",
     "niss_recibido": datos.niss,
@@ -26,12 +25,10 @@ return {
     "gfmf_recibido": datos.gfmf,
     "operacional_recibido": datos.operacional
 }
-```
 
 @app.post("/subir_pdf")
 async def subir_pdf(archivo: UploadFile = File(...)):
 
-```
 contenido = await archivo.read()
 
 total_paginas = contar_paginas_pdf(contenido)
@@ -41,12 +38,10 @@ return {
     "nombre_archivo": archivo.filename,
     "paginas": total_paginas
 }
-```
 
 @app.post("/subir_imagen")
 async def subir_imagen(archivo: UploadFile = File(...)):
 
-```
 contenido = await archivo.read()
 
 texto_extraido = analizar_imagen(contenido)
@@ -56,14 +51,13 @@ return {
     "nombre_archivo": archivo.filename,
     "texto_extraido": texto_extraido
 }
-```
 
 @app.post("/analizar_inspeccion")
 async def analizar_inspeccion(archivo: UploadFile = File(...)):
 
-```
 contenido = await archivo.read()
 
 datos_inspeccion = analizar_imagen(contenido)
 
 return datos_inspeccion
+
