81070383f335f15c603e2a070d47dcef3fe68034
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-11 10:20:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Modelo que recibirá la información enviada desde Power Automate
+class DatosEntrada(BaseModel):
+    texto: str
+
+# Ruta de prueba
 @app.get("/")
 def inicio():
     return {"mensaje": "Hola desde PaytowinSpace"}
+
+# Ruta para recibir información desde Power Automate
+@app.post("/procesar")
+def procesar(datos: DatosEntrada):
+    return {
+        "mensaje": "Datos recibidos correctamente",
+        "texto_recibido": datos.texto
+    }
