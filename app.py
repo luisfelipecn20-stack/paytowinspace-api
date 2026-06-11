8a30a99453ac85a9cb5068f3940700b6e6670d13
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-11 11:05:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
+import fitz  # PyMuPDF
 
 app = FastAPI()
 
 
-# Modelo que recibirá información desde Power Automate
 class DatosEntrada(BaseModel):
     niss: str = ""
     inspeccion: str = ""
@@ -12,13 +12,11 @@ class DatosEntrada(BaseModel):
     operacional: str = ""
 
 
-# Ruta de prueba
 @app.get("/")
 def inicio():
     return {"mensaje": "Hola desde PaytowinSpace"}
 
 
-# Ruta para procesar información enviada como JSON
 @app.post("/procesar")
 def procesar(datos: DatosEntrada):
 
@@ -31,14 +29,18 @@ def procesar(datos: DatosEntrada):
     }
 
 
-# Ruta para recibir un PDF
 @app.post("/subir_pdf")
 async def subir_pdf(archivo: UploadFile = File(...)):
 
     contenido = await archivo.read()
 
+    # Abrir PDF
+    pdf = fitz.open(stream=contenido, filetype="pdf")
+
+    total_paginas = len(pdf)
+
     return {
         "resultado": "OK",
         "nombre_archivo": archivo.filename,
-        "tamano_bytes": len(contenido)
+        "paginas": total_paginas
     }
