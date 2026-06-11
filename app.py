2c41aea799fe01c80face0ca3328a40457375485
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-11 11:01:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-# Modelo que recibirá la información desde Power Automate
+# Modelo que recibirá información desde Power Automate
 class DatosEntrada(BaseModel):
     niss: str = ""
     inspeccion: str = ""
@@ -18,7 +18,7 @@ def inicio():
     return {"mensaje": "Hola desde PaytowinSpace"}
 
 
-# Ruta para procesar la información
+# Ruta para procesar información enviada como JSON
 @app.post("/procesar")
 def procesar(datos: DatosEntrada):
 
@@ -28,4 +28,17 @@ def procesar(datos: DatosEntrada):
         "inspeccion_recibida": datos.inspeccion,
         "gfmf_recibido": datos.gfmf,
         "operacional_recibido": datos.operacional
+    }
+
+
+# Ruta para recibir un PDF
+@app.post("/subir_pdf")
+async def subir_pdf(archivo: UploadFile = File(...)):
+
+    contenido = await archivo.read()
+
+    return {
+        "resultado": "OK",
+        "nombre_archivo": archivo.filename,
+        "tamano_bytes": len(contenido)
     }
