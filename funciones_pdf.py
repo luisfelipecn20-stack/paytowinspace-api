e199e22c5ce93dd6618f8fc817cad4924d566836
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-11 11:18:21 -0500

    Update funciones_pdf.py

--- funciones_pdf.py
+++ funciones_pdf.py
@@ -5,7 +5,21 @@ def contar_paginas_pdf(contenido_pdf):
 
     pdf = fitz.open(stream=contenido_pdf, filetype="pdf")
 
-
-
-  
     return len(pdf)
+
+
+def convertir_pdf_a_imagenes(contenido_pdf):
+
+    pdf = fitz.open(stream=contenido_pdf, filetype="pdf")
+
+    imagenes = []
+
+    for pagina in pdf:
+
+        pix = pagina.get_pixmap()
+
+        imagen = pix.tobytes("png")
+
+        imagenes.append(imagen)
+
+    return imagenes
