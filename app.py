f97717648daad4293c3719f25db9ecb0447d4c37
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-11 14:13:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,271 +5,66 @@ from funciones_vision import analizar_imagen
 
 app = FastAPI()
 
-
 class DatosEntrada(BaseModel):
-    niss: str = ""
-    inspeccion: str = ""
-    gfmf: str = ""
-    operacional: str = ""
-
+niss: str = ""
+inspeccion: str = ""
+gfmf: str = ""
+operacional: str = ""
 
 @app.get("/")
 def inicio():
-    return {"mensaje": "Hola desde PaytowinSpace"}
-
+return {"mensaje": "Hola desde PaytowinSpace"}
 
 @app.post("/procesar")
 def procesar(datos: DatosEntrada):
 
-    return {
-        "resultado": "OK",
-        "niss_recibido": datos.niss,
-        "inspeccion_recibida": datos.inspeccion,
-        "gfmf_recibido": datos.gfmf,
-        "operacional_recibido": datos.operacional
-    }
-
-
-@app.post("/subir_pdf")
-async def subir_pdf(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    total_paginas = contar_paginas_pdf(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "paginas": total_paginas
-    }
-
-
-@app.post("/subir_imagen")
-async def subir_imagen(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    texto_extraido = analizar_imagen(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "texto_extraido": texto_extraido
-    }
-
-
-@app.post("/analizar_inspeccion")
-async def analizar_inspeccion(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    datos_inspeccion = analizar_imagen(contenido)
-
-    return datos_inspeccion
 ```
-from fastapi import FastAPI, UploadFile, File
-from pydantic import BaseModel
-from funciones_pdf import contar_paginas_pdf
-from funciones_vision import analizar_imagen
-
-app = FastAPI()
-
-
-class DatosEntrada(BaseModel):
-    niss: str = ""
-    inspeccion: str = ""
-    gfmf: str = ""
-    operacional: str = ""
-
-
-@app.get("/")
-def inicio():
-    return {"mensaje": "Hola desde PaytowinSpace"}
-
-
-@app.post("/procesar")
-def procesar(datos: DatosEntrada):
-
-    return {
-        "resultado": "OK",
-        "niss_recibido": datos.niss,
-        "inspeccion_recibida": datos.inspeccion,
-        "gfmf_recibido": datos.gfmf,
-        "operacional_recibido": datos.operacional
-    }
-
-
-@app.post("/subir_pdf")
-async def subir_pdf(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    total_paginas = contar_paginas_pdf(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "paginas": total_paginas
-    }
-
-
-@app.post("/subir_imagen")
-async def subir_imagen(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    texto_extraido = analizar_imagen(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "texto_extraido": texto_extraido
-    }
-
-
-@app.post("/analizar_inspeccion")
-async def analizar_inspeccion(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    datos_inspeccion = analizar_imagen(contenido)
-
-    return datos_inspeccion
+return {
+    "resultado": "OK",
+    "niss_recibido": datos.niss,
+    "inspeccion_recibida": datos.inspeccion,
+    "gfmf_recibido": datos.gfmf,
+    "operacional_recibido": datos.operacional
+}
 ```
-from fastapi import FastAPI, UploadFile, File
-from pydantic import BaseModel
-from funciones_pdf import contar_paginas_pdf
-from funciones_vision import analizar_imagen
-
-app = FastAPI()
-
-
-class DatosEntrada(BaseModel):
-    niss: str = ""
-    inspeccion: str = ""
-    gfmf: str = ""
-    operacional: str = ""
-
-
-@app.get("/")
-def inicio():
-    return {"mensaje": "Hola desde PaytowinSpace"}
-
-
-@app.post("/procesar")
-def procesar(datos: DatosEntrada):
-
-    return {
-        "resultado": "OK",
-        "niss_recibido": datos.niss,
-        "inspeccion_recibida": datos.inspeccion,
-        "gfmf_recibido": datos.gfmf,
-        "operacional_recibido": datos.operacional
-    }
-
 
 @app.post("/subir_pdf")
 async def subir_pdf(archivo: UploadFile = File(...)):
 
-    contenido = await archivo.read()
-
-    total_paginas = contar_paginas_pdf(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "paginas": total_paginas
-    }
-
-
-@app.post("/subir_imagen")
-async def subir_imagen(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    texto_extraido = analizar_imagen(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "texto_extraido": texto_extraido
-    }
-
-
-@app.post("/analizar_inspeccion")
-async def analizar_inspeccion(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    datos_inspeccion = analizar_imagen(contenido)
-
-    return datos_inspeccion
 ```
-from fastapi import FastAPI, UploadFile, File
-from pydantic import BaseModel
-from funciones_pdf import contar_paginas_pdf
-from funciones_vision import analizar_imagen
+contenido = await archivo.read()
 
-app = FastAPI()
+total_paginas = contar_paginas_pdf(contenido)
 
-
-class DatosEntrada(BaseModel):
-    niss: str = ""
-    inspeccion: str = ""
-    gfmf: str = ""
-    operacional: str = ""
-
-
-@app.get("/")
-def inicio():
-    return {"mensaje": "Hola desde PaytowinSpace"}
-
-
-@app.post("/procesar")
-def procesar(datos: DatosEntrada):
-
-    return {
-        "resultado": "OK",
-        "niss_recibido": datos.niss,
-        "inspeccion_recibida": datos.inspeccion,
-        "gfmf_recibido": datos.gfmf,
-        "operacional_recibido": datos.operacional
-    }
-
-
-@app.post("/subir_pdf")
-async def subir_pdf(archivo: UploadFile = File(...)):
-
-    contenido = await archivo.read()
-
-    total_paginas = contar_paginas_pdf(contenido)
-
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "paginas": total_paginas
-    }
-
+return {
+    "resultado": "OK",
+    "nombre_archivo": archivo.filename,
+    "paginas": total_paginas
+}
+```
 
 @app.post("/subir_imagen")
 async def subir_imagen(archivo: UploadFile = File(...)):
 
-    contenido = await archivo.read()
+```
+contenido = await archivo.read()
 
-    texto_extraido = analizar_imagen(contenido)
+texto_extraido = analizar_imagen(contenido)
 
-    return {
-        "resultado": "OK",
-        "nombre_archivo": archivo.filename,
-        "texto_extraido": texto_extraido
-    }
-
+return {
+    "resultado": "OK",
+    "nombre_archivo": archivo.filename,
+    "texto_extraido": texto_extraido
+}
+```
 
 @app.post("/analizar_inspeccion")
 async def analizar_inspeccion(archivo: UploadFile = File(...)):
 
-    contenido = await archivo.read()
+```
+contenido = await archivo.read()
 
-    datos_inspeccion = analizar_imagen(contenido)
+datos_inspeccion = analizar_imagen(contenido)
 
-    return datos_inspeccion
+return datos_inspeccion
+```
