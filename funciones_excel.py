4ffae5a380b6892a678f20b65bf4c8357a6f62ad
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-12 15:17:07 -0500

    Update funciones_excel.py

--- funciones_excel.py
+++ funciones_excel.py
@@ -1,37 +1,10 @@
-import pandas as pd
-
-# Columnas que necesita la resolución
-COLUMNAS_RESOLUCION = [
-    "medidor",
-    "nis_rad",
-    "num_os",
-    "fec_vis",
-    "serv",
-    "cx_c_dt",
-    "lec",
-    "fuga_caj",
-    "uu_ocup",
-    "soc_ocup",
-    "dom_ocup",
-    "com_ocup",
-    "ind_ocup",
-    "est_ocup",
-    "uu_docup",
-    "soc_desc",
-    "dom_desc",
-    "com_desc",
-    "ind_desc",
-    "est_desc",
-    "observ",
-    "observm1"
-]
-
-
 def crear_excel_maestro(excel_bruto):
 
     df_bruto = pd.read_excel(excel_bruto)
 
-    df_maestro = df_bruto.reindex(columns=COLUMNAS_RESOLUCION)
+    df_maestro = df_bruto.reindex(
+        columns=COLUMNAS_RESOLUCION
+    )
 
     return df_maestro
 
@@ -54,7 +27,10 @@ def obtener_datos_resolucion(excel_bruto):
 
     df_bruto = pd.read_excel(excel_bruto)
 
-    df_resolucion = df_bruto.reindex(columns=COLUMNAS_RESOLUCION)
+    df_resolucion = df_bruto.reindex(
+        columns=COLUMNAS_RESOLUCION
+    )
 
-    return df_resolucion.to_dict(orient="records")
-    
+    return df_resolucion.to_dict(
+        orient="records"
+    )
