a619cdbdedd25b240a09581440efddef9bbbd1f0
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-12 14:09:50 -0500

    Update funciones_excel.py

--- funciones_excel.py
+++ funciones_excel.py
@@ -1,58 +1,55 @@
+```python
 import pandas as pd
 
+# Columnas que necesita la resolución
+COLUMNAS_RESOLUCION = [
+    "medidor",
+    "nis_rad",
+    "num_os",
+    "fec_vis",
+    "serv",
+    "cx_c_dt",
+    "lec",
+    "fuga_caj",
+    "uu_ocup",
+    "soc_ocup",
+    "dom_ocup",
+    "com_ocup",
+    "ind_ocup",
+    "est_ocup",
+    "uu_docup",
+    "soc_desc",
+    "dom_desc",
+    "com_desc",
+    "ind_desc",
+    "est_desc",
+    "observ",
+    "observm1"
+]
 
-def crear_excel_maestro(excel_bruto, excel_plantilla):
+
+def crear_excel_maestro(excel_bruto):
 
     df_bruto = pd.read_excel(excel_bruto)
 
-    df_plantilla = pd.read_excel(excel_plantilla)
+    # Conserva únicamente las columnas necesarias
+    # y en el orden definido
+    df_maestro = df_bruto.reindex(columns=COLUMNAS_RESOLUCION)
 
-    encabezados_maestros = df_plantilla.columns.tolist()
-
-    equivalencias = {
-
-        "medidor": "medidor",
-        "nis_rad": "nis_rad",
-        "num_os": "num_os",
-        "fec_vis": "fec_vis",
-        "serv": "serv",
-        "cx_c_dt": "cx_c_dt",
-        "lec": "lec",
-        "fuga_caj": "fuga_caj",
-        "uu_ocup": "uu_ocup",
-        "soc_ocup": "soc_ocup",
-        "dom_ocup": "dom_ocup",
-        "com_ocup": "com_ocup",
-        "ind_ocup": "ind_ocup",
-        "est_ocup": "est_ocup",
-        "uu_docup": "uu_docup",
-        "soc_desc": "soc_desc",
-        "dom_desc": "dom_desc",
-        "com_desc": "com_desc",
-        "ind_desc": "ind_desc",
-        "est_desc": "est_desc",
-        "observ": "observ",
-        "observm1": "observm1"
-
-    }
-
-    df_maestro = df_bruto.rename(columns=equivalencias)
-
-    df_maestro = df_maestro[encabezados_maestros]
-    
     return df_maestro
+
 
 def guardar_excel_maestro(
         excel_bruto,
-        excel_plantilla,
         ruta_salida):
 
     df_maestro = crear_excel_maestro(
-        excel_bruto,
-        excel_plantilla
+        excel_bruto
     )
 
     df_maestro.to_excel(
         ruta_salida,
         index=False
     )
+```
+
