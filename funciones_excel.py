d1939b0f68c9eb7fd47648bb94cc17ad76ae3d6f
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-12 11:46:11 -0500

    Update funciones_excel.py

--- funciones_excel.py
+++ funciones_excel.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
 
-def crear_excel_maestro():
-    pass
+def crear_excel_maestro(excel_bruto, excel_plantilla):
+
+    df_bruto = pd.read_excel(excel_bruto)
+
+    df_plantilla = pd.read_excel(excel_plantilla)
+
+    encabezados_maestros = df_plantilla.columns.tolist()
+
+    return encabezados_maestros
