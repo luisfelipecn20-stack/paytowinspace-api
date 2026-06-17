2fc9362d6876429e9ed0f41b4c70ec894ace34e0
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-17 16:09:52 -0500

    Update funciones_formato_2.py

--- funciones_formato_2.py
+++ funciones_formato_2.py
@@ -4,20 +4,33 @@ def obtener_datos_formato_2(pdf_formato_2):
 
     datos_formato_2 = {
 
-        "fecha_reclamo": "",
-        "medio_presentacion": "",
+        # Identificación del expediente
+        "re": "",
+        "niss": "",
+
+        # Usuario
+        "reclamante": "",
+
+        # Direcciones
+        "direccion_suministro": "",
+        "direccion_procesal": "",
+
+        # Correo electrónico
+        "tiene_correo": "",
+        "correo_electronico": "",
+
+        # Reclamo
         "tipo_reclamo": "",
         "mes_reclamado": "",
 
-        "suministro": "",
-        "reclamante": "",
+        # Canal de atención
+        "canal_atencion": "",
 
-        "direccion_suministro": "",
-        "direccion_procesal": "",
-        "correo_electronico": "",
+        # Audiencia
+        "fecha_audiencia": "",
 
-        "fecha_inspeccion": "",
-        "fecha_audiencia": ""
+        # Contraste
+        "solicita_contraste": ""
 
     }
 
