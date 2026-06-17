e75510d9bce58d10665ab66ef3ea93a98d4922f0
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-17 15:48:16 -0500

    Create funciones_formato_2.py

---
+++ funciones_formato_2.py
@@ -0,0 +1,24 @@
+# funciones_formato_2.py
+
+def obtener_datos_formato_2(pdf_formato_2):
+
+    datos_formato_2 = {
+
+        "fecha_reclamo": "",
+        "medio_presentacion": "",
+        "tipo_reclamo": "",
+        "mes_reclamado": "",
+
+        "suministro": "",
+        "reclamante": "",
+
+        "direccion_suministro": "",
+        "direccion_procesal": "",
+        "correo_electronico": "",
+
+        "fecha_inspeccion": "",
+        "fecha_audiencia": ""
+
+    }
+
+    return datos_formato_2
