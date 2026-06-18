0a921dd137c988f7c88363a2668713dd3eb1da30
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-06-18 16:20:59 -0500

    Update funciones_vision.py

--- funciones_vision.py
+++ funciones_vision.py
@@ -7,11 +7,29 @@ cliente = OpenAI(
 )
 
 
-def analizar_imagen(imagen_png, prompt_sistema):
+def analizar_imagen(imagenes_png, prompt_sistema):
 
-    imagen_base64 = base64.b64encode(
-        imagen_png
-    ).decode("utf-8")
+    contenido = [
+        {
+            "type": "text",
+            "text": "Analiza las siguientes imágenes."
+        }
+    ]
+
+    for imagen_png in imagenes_png:
+
+        imagen_base64 = base64.b64encode(
+            imagen_png
+        ).decode("utf-8")
+
+        contenido.append(
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/png;base64,{imagen_base64}"
+                }
+            }
+        )
 
     respuesta = cliente.chat.completions.create(
         model="gpt-4o-mini",
@@ -22,18 +40,7 @@ def analizar_imagen(imagen_png, prompt_sistema):
             },
             {
                 "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Analiza esta imagen."
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": f"data:image/png;base64,{imagen_base64}"
-                        }
-                    }
-                ]
+                "content": contenido
             }
         ]
     )
