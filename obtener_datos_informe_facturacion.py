2cdbfd1a405a749c51a5654084816a41b4ff3537
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-07-05 01:38:48 -0500

    Update obtener_datos_informe_facturacion.py

--- obtener_datos_informe_facturacion.py
+++ obtener_datos_informe_facturacion.py
@@ -2,7 +2,8 @@ from funciones_pdf import convertir_pdf_a_imagenes
 from funciones_vision import analizar_imagen
 from prompt_informe_facturacion import PROMPT_INFORME_FACTURACION
 
-    def obtener_datos_informe_facturacion(pdf):
+
+def obtener_datos_informe_facturacion(pdf):
 
     imagenes = convertir_pdf_a_imagenes(
         pdf
