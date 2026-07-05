22c2671c29325b257f9e42f3f6317a56fbf21c8a
luisfelipecn20-stack <luisfelipecn20@gmail.com>
2026-07-05 00:41:03 -0500

    funciones_considerando_3.py

---
+++ funciones_considerando_3.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def generar_considerando_3(datos):
+    """
+    Genera el texto del Considerando Tercero utilizando
+    la información previamente procesada por la API.
+    """
+
+    pass
